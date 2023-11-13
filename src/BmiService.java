8a1bea266c00e62ba2aa459757bed6bf6bdbbfd7
Anton Sergeev <sergant3082605@mail.com>
2023-11-13 23:42:01 +0400

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    int calculate (double heightInMeters, double weight) {
-        return (int)(weight / Math.pow(heightInMeters, 2));
+    int calculate(double heightInMeters, double weight) {
+        return (int) (weight / Math.pow(heightInMeters, 2));
     }
 }
